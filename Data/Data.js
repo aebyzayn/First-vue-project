1db96425523e681681304343f468b28d61cab82f
The Beg Joker <115506566+thebegjoker@users.noreply.github.com>
2023-11-14 18:35:56 +0600

    Add files via upload

---
+++ Data/Data.js
@@ -0,0 +1,105 @@
+// Header Data.................
+import HeaderImage from '../assets/HeaderImage.png'
+ export const  headerData =[
+    {
+        id: 1,
+        title: 'Vue Landing Page Template',
+        desc:'This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com',
+        btnLink: '#',
+        image: HeaderImage,
+    }
+];
+
+
+
+
+// Product Data.................
+import ProductImage from '../assets/ProductImage.png'
+ export const  productData =[
+    {
+        id: 1,        
+        image: ProductImage,
+    }
+];
+
+
+
+// Feature Data .................
+import CardImage1 from '../assets/Cardsicon1.svg'
+import CardImage2 from '../assets/Cardicon2.svg'
+import CardImage3 from '../assets/Cardicon3.svg'
+ export const  featureData =[
+    {
+        id: 1,
+        title: 'Customizable card',
+        desc:'Custom your own card for your exact incomes and expenses needs.',     
+        image: CardImage1,
+    },
+    {
+        id: 1,
+        title: 'No payment fee',
+        desc:'Transfer your payment all over the world with no payment fee.',       
+        image: CardImage2,
+    },
+    {
+        id: 1,
+        title: 'All in one place',
+        desc:'The right place to keep your credit and debit cards, boarding passes & more.  ',      
+        image: CardImage3,
+    },
+];
+
+
+
+
+// Testimonials Data Data .................
+import testimonialsImage1 from '../assets/testimonial1.png'
+import testimonialsImage2 from '../assets/testimonials2.png'
+import testimonialsImage3 from '../assets/testimonials3.jpg'
+ export const  testimonialsData =[
+    {
+        id: 1,
+        name: 'Johnny Owens',
+        desc:"“Wallet is a great product! All of my most important information is there - credit cards, transit cards, boarding passes, tickets, and more. And I don't need to worry because it's all in one place! thanks!”",
+        image: testimonialsImage1,
+    },  
+    {
+        id: 2,
+        name: 'Johnny Owens',
+        desc:"“Wallet is a great product! All of my most important information is there - credit cards, transit cards, boarding passes, tickets, and more. And I don't need to worry because it's all in one place! thanks!”",        
+        image: testimonialsImage2,
+    },  
+    {
+        id: 3,
+        name: 'Johnny Owens',
+        desc:"“Wallet is a great product! All of my most important information is there - credit cards, transit cards, boarding passes, tickets, and more. And I don't need to worry because it's all in one place! thanks!”",
+        image: testimonialsImage3,
+    },  
+   
+];
+
+
+
+// Testimonials  Data .................
+import ContactImage from '../assets/Contact.png'
+ export const  cotactData =[
+    {
+        id: 1,
+        title: "Questions? Let’s talk ",
+        desc:"Contact us through our 24/7 live chat.We’re always happy to help!",
+        btnLink: '#',
+        image: ContactImage,
+    },
+];
+
+
+
+
+
+// Footer Data .................
+export const  footerData =[
+    { id: 1, name: '© Vuelet 2024', link: '#' },
+    { id: 2, name: 'Privacy policy', link: '#' },
+    { id: 3, name: 'Cookies policy', link: '#' },
+    { id: 3, name: 'Terms of use', link: '#' },
+]
